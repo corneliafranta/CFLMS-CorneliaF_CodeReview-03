133395bceb7f348c37900e3661fc2fc68f7ed27a
Cornelia Franta <franta-cornelia@gmx.at>
2020-05-30 01:01:25 +0200

    adding codeRevie3

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,48 @@
+
+function calculateInsurance() {
+
+    //getting all the values
+
+    var name = document.getElementById('name').value;
+    var age = Number(document.getElementById('age').value);
+    var country = document.getElementById('countrySel').value;
+    var horsepower = Number(document.getElementById('horsepower').value);
+
+    //calculating quote
+
+    var rate = 0;
+
+    if (country === 'austria'){
+        rate = (horsepower * 100) / (age + 50);
+    }else if (country === 'hungary'){
+        rate = (horsepower *120) / (age + 100);
+    }else{
+        rate = (horsepower *150) / ((age + 3)+50);
+    }
+
+    console.log(rate);
+    rate = rate.toFixed(1);
+
+
+    var resultField = document.getElementById('result');
+    resultField.innerHTML= `${name}, your insurance costs ${rate} €`;
+    resultField.style.display = 'flex';
+
+
+    if (window.matchMedia('(max-width: 768px)').matches){
+        console.log(window.matchMedia('(max-width: 768px)'));
+        console.log(30);
+        document.getElementById('calcWindow').style.height = '55em';
+        document.getElementById('opagewindow').style.height = '55em';
+        document.getElementById('main').style.height = '70em';
+    }
+
+
+
+}
+
+var submit = document.getElementById('calculate');
+submit.addEventListener('click', calculateInsurance);
+
+
+
---
+++ JAVASCRIPT/team..js
@@ -0,0 +1,133 @@
+var teamContainer = document.getElementById('team');
+var teamDetailContainer = document.getElementById('teamDetail');
+
+class TeamMember {
+
+    constructor(teamMember) {
+        this.firstName = teamMember.firstName;
+        this.lastName = teamMember.lastName;
+        this.image = teamMember.image;
+        this.email = teamMember.email;
+        this.telephone = teamMember.telephone;
+        this.profession = teamMember.profession;
+        this.createMemberEntry()
+    }
+
+
+    createMemberDetail() {
+
+
+        var teamMemberObject = teamMemberObjects[this.id];
+        document.getElementById('TeamMain').style.height='60em';
+        teamDetailContainer.innerHTML = "";
+        teamDetailContainer.style.display = 'flex';
+        var memberDetailRow = document.createElement('div');
+        memberDetailRow.setAttribute('class', 'row');
+
+        var imageDiv = document.createElement('div');
+        imageDiv.setAttribute('id', 'teamDetailImage');
+
+        var image = document.createElement('img');
+        image.setAttribute('src', `./MEDIA/${teamMemberObject.image}`);
+        image.setAttribute('alt', `Picture ${teamMemberObject.firstName} `);
+
+        imageDiv.appendChild(image);
+        memberDetailRow.appendChild(imageDiv);
+
+        var detail = document.createElement('div');
+        detail.setAttribute('id', 'employeeDetail');
+
+        var detailHeading = document.createElement('span');
+        detailHeading.setAttribute('id', 'employeeDetailHeading');
+        detailHeading.innerHTML = `${teamMemberObject.firstName} ${teamMemberObject.lastName}`;
+
+        var detailProfession = document.createElement('span');
+        detailProfession.setAttribute('id', 'employeeDetailProfession');
+        detailProfession.innerHTML = teamMemberObject.profession;
+
+        var detailContactHeader = document.createElement('h3');
+        detailContactHeader.innerHTML = 'How to contact me';
+
+        var detailContactPhone = document.createElement('span');
+        detailContactPhone.setAttribute('class', 'employeeDetailContact');
+        detailContactPhone.innerHTML = `Telephone: ${teamMemberObject.telephone}`;
+
+        var detailContactEmail = document.createElement('span');
+        detailContactEmail.setAttribute('class', 'employeeDetailContact');
+        detailContactEmail.innerHTML = `Email: ${teamMemberObject.email}`;
+
+        detail.appendChild(detailHeading);
+        detail.appendChild(detailProfession);
+        detail.appendChild(detailContactHeader);
+        detail.appendChild(detailContactPhone);
+        detail.appendChild(detailContactEmail);
+        memberDetailRow.appendChild(detail);
+        teamDetailContainer.appendChild(memberDetailRow);
+    }
+
+    createMemberEntry() {
+        var teamElement = document.createElement('div');
+        teamElement.setAttribute('id', `teamElement${this.firstName}`);
+        teamElement.setAttribute('class', 'teamElement');
+
+        var image = document.createElement('img');
+        image.setAttribute('src', `./MEDIA/${this.image}`);
+        image.setAttribute('alt', `Picture ${this.firstName} `);
+
+        var name = document.createElement('span');
+        name.setAttribute('class', 'memberName');
+        name.innerHTML = `${this.firstName} ${this.lastName}`;
+
+        var profession = document.createElement('span');
+        profession.setAttribute('class', 'memberProfession');
+        profession.innerHTML = `${this.profession}`;
+
+        var button = document.createElement('button');
+        button.setAttribute('type', 'button');
+        button.innerHTML = 'learn more';
+        button.addEventListener('click', this.createMemberDetail);
+        button.setAttribute("id", this.firstName);
+
+        teamElement.appendChild(image);
+        teamElement.appendChild(name);
+        teamElement.appendChild(profession);
+        teamElement.appendChild(button);
+        teamContainer.appendChild(teamElement);
+
+
+    }
+
+
+}
+
+
+var teamMembers = [
+    {
+        firstName: 'Anna',
+        lastName: 'Miller',
+        profession: 'Customer Service',
+        image: 'employeeanna.jpg',
+        telephone: '+43 680 208 563',
+        email: 'anna@insurancecf.net'
+    },
+    {
+        firstName: 'Tim',
+        lastName: 'Bauer',
+        profession: 'CEO',
+        image: 'employeetim.jpg',
+        telephone: '+43 680 208 583',
+        email: 'tim@insurancecf.net'
+    },
+    {
+        firstName: 'Laura',
+        lastName: 'Turner',
+        profession: 'Lawyer',
+        image: 'employeelaura.jpg',
+        telephone: '+43 680 208 593',
+        email: 'laura@insurancecf.net'
+    }
+];
+teamMemberObjects = {};
+teamMembers.forEach(function (teamMember) {
+    teamMemberObjects[teamMember.firstName] = new TeamMember(teamMember);
+});
